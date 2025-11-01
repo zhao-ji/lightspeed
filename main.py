dbc8ea06a872fc9d6a6829f3d9a0bce538a200b4
Subs <BytesWarning@gmail.com>
2025-11-02 11:32:27 +1300

    stage 2

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from flask import Flask, jsonify, request
-import sqlite3
 import os
+import sqlite3
+
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -22,13 +23,33 @@ def get_db_connection():
     conn.row_factory = sqlite3.Row
     return conn
 
+def serialize(rows):
+    data = [dict(row) for row in rows]
+    return jsonify(data)
+
 @app.route('/products', methods=['GET'])
 def get_products():
     """Get current products"""
     conn = get_db_connection()
     products = conn.execute('SELECT id, name, price FROM products').fetchall()
     conn.close()
-    return jsonify(products)
+    return serialize(products)
+
+@app.route('/products', methods=['POST'])
+def add_products():
+    new_product = request.json
+    if "name" not in new_product or not new_product["name"]:
+        return jsonify({"error": "name should not be empty"}), 400
+    if "price" not in new_product or not new_product["price"]:
+        return jsonify({"error": "price should not be empty"}), 400
+
+    conn = get_db_connection()
+    data = (new_product["name"], new_product["price"])
+    conn.execute('INSERT INTO products(name, price) values(?, ?)', data)
+    conn.commit()
+    conn.close()
+
+    return jsonify(new_product), 201 # 201 Created
 
 def main():
     init_database()
